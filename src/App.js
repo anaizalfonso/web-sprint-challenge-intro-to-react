bed05d10552ddd62270fd8713d561897ed080e8f
anaiz.alfonso <anaiz.alfonso@hotmai.com>
2022-12-15 22:57:48 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import character from './components/Character';
 
 const App = () => {
+
+
+  const [namePeople, setNamePeople] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setNamePeople(res.data);
+    })
+     
+    .catch(err => {
+      console.error(err)
+    })
+  }, []);
+
+  const character = namePeople.map(inner => inner.name + inner.height + inner.mass + inner.hair_color + inner.gender)
+  return (
+    <div className="App">
+      <h1 className="Header">character</h1>
+      {character}
+
+      </div>
+    );
+  }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,11 +35,5 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React  from "react";
+
+export default function character ({props}){
+
+const [character, setDetails] = useState(null)
+useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setNamePeople(res.data);
+    })
+     
+    .catch(err => {
+      console.error(err)
+    })
+  }, []);
+
+return (
+    <div className='container'>
+    </div>
+)
+}
+    
